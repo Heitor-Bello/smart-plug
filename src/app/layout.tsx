258192c7a247193dfac4386092c134a75c31aaaa
Heitor-Bello <heitor.augustob@hotmail.com>
2026-03-19 20:16:58 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "VoltMetric",
+  description:
+    "Controle o consumo energético de seu equipamento eletrônico com o VoltMetric",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${manrope.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
